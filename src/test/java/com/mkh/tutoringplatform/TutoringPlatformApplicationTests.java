12b1611f4ca7ac9a8db731f5e4c447f0384ba9f6
mykola <mykola.khlopyk@gmail.com>
2023-05-05 17:08:33 +0300

    create start spring project

---
+++ src/test/java/com/mkh/tutoringplatform/TutoringPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkh.tutoringplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoringPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
