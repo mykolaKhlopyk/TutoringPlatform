c4bd7574b934e4181083c69542f676da7fd0f909
mykola <mykola.khlopyk@gmail.com>
2023-05-15 12:41:04 +0300

    user repository by using Jpa

---
+++ src/main/java/com/mkh/tutoringplatform/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.mkh.tutoringplatform.repository;
+
+import com.mkh.tutoringplatform.domain.user.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
