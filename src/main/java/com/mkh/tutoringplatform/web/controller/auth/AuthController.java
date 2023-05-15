0c91d8b6d1a739baa97ca07d0657ae23d5489200
mykola <mykola.khlopyk@gmail.com>
2023-05-15 23:26:26 +0300

    add methods for registration in AuthController

--- src/main/java/com/mkh/tutoringplatform/web/controller/auth/AuthController.java
+++ src/main/java/com/mkh/tutoringplatform/web/controller/auth/AuthController.java
@@ -1,15 +1,41 @@
 package com.mkh.tutoringplatform.web.controller.auth;
 
+import com.mkh.tutoringplatform.domain.user.user.User;
+import com.mkh.tutoringplatform.service.RegistrationService;
+import com.mkh.tutoringplatform.web.validaton.UserValidator;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
 
 @Controller
+@AllArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
 
-    @GetMapping("login")
+    private final UserValidator userValidator;
+    private final RegistrationService registrationService;
+
+    @GetMapping("/login")
     public String loginPage(){
         return "auth/login";
+    }
+
+    @GetMapping("/registration")
+    public String registrationPage(@ModelAttribute("user") User user){
+        return "auth/registration";
+    }
+
+    @PostMapping("/registration")
+    public String performRegistration(@ModelAttribute("user") @Valid User user, BindingResult bindingResult){
+        userValidator.validate(user, bindingResult);
+        if (bindingResult.hasErrors())
+            return "/auth/registration";
+        registrationService.register(user);
+        return "redirect:/auth/login";
     }
 }
