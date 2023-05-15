f0558e5a0527477e5be26f844b9701d6daf09bd4
mykola <mykola.khlopyk@gmail.com>
2023-05-15 23:22:02 +0300

    solve problem with validation

--- src/main/java/com/mkh/tutoringplatform/domain/user/user/User.java
+++ src/main/java/com/mkh/tutoringplatform/domain/user/user/User.java
@@ -1,14 +1,12 @@
 package com.mkh.tutoringplatform.domain.user.user;
 
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import lombok.Data;
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.UniqueElements;
 
+
+
+import lombok.Data;
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.util.Set;
 
 
@@ -22,28 +20,29 @@ public class User {
     private Long id;
 
     @NotNull
-    @Email
+    @Email(message = "email is incorrect")
     private String email;
 
     @NotNull
-    @Length(min = 6, max = 16, message = "Username should be between 6 and 16 characters")
-    @Pattern(regexp = "(?<![^\\W_])\\b[a-z]+(?:_[a-z]+)*\\b(?![^\\W_])")
+    @Size(min = 6, max = 16, message = "Username should be between 6 and 16 characters")
+    @Pattern(regexp = "(?<![^\\W_])\\b[a-z]+(?:_[a-z]+)*\\b(?![^\\W_])", message = "username contains only small characters and _ between them")
     private String username;
 
     @NotNull
-    @Length(min = 6, max = 16, message = "Password should be between 6 and 16 characters")
+    @NotEmpty(message = "not empty")
+    @Size(min = 6, max = 16, message = "Password should be between 6 and 16 characters")
     private String password;
 
     @NotNull
-    @Length(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
 
     @NotNull
-    @Length(min = 2, max = 30, message = "Surname should be between 2 and 30 characters")
+    @Size(min = 2, max = 30, message = "Surname should be between 2 and 30 characters")
     private String surname;
 
     @NotNull
-    @Length(min = 2, max = 30, message = "Phone number should be between 2 and 30 characters")
+    @Size(min = 2, max = 30, message = "Phone number should be between 2 and 30 characters")
     private String phone;
 
     @Enumerated(EnumType.STRING)
