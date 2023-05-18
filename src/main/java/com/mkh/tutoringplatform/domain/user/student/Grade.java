9065616c9a4f293a1a84c5fcb77fbee4fd3a96b5
mykola <mykola.khlopyk@gmail.com>
2023-05-18 13:33:13 +0300

    add grades for students and suitable for teacher

---
+++ src/main/java/com/mkh/tutoringplatform/domain/user/student/Grade.java
@@ -0,0 +1,10 @@
+package com.mkh.tutoringplatform.domain.user.student;
+
+public enum Grade {
+    BEFORE_SCHOOL,
+    ELEMENTARY_SCHOOL,
+    SECONDARY_SCHOOL,
+    HIGH_SCHOOL,
+    UNIVERSITY,
+    ALL
+}
