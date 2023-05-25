53bd57749256c0155bc9d6b2a6b1b006903131b2
mykola <mykola.khlopyk@gmail.com>
2023-05-25 22:49:07 +0300

    interface for registration

--- src/main/java/com/mkh/tutoringplatform/service/RegistrationService.java
+++ src/main/java/com/mkh/tutoringplatform/service/RegistrationService.java
@@ -1,7 +1,11 @@
 package com.mkh.tutoringplatform.service;
 
+import com.mkh.tutoringplatform.domain.user.student.Student;
+import com.mkh.tutoringplatform.domain.user.teacher.Teacher;
 import com.mkh.tutoringplatform.domain.user.user.User;
 
 public interface RegistrationService {
-    void register(User user);
+    void registerUser(User user);
+    void registerTeacher(Teacher teacher);
+    void registerStudent(Student student);
 }
