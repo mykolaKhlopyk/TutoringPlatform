1211a0326942229dc0c721d8706d0b19d7ba69b8
mykola <mykola.khlopyk@gmail.com>
2023-05-25 22:54:57 +0300

    add get and post methods for creating teacher and student + update references. Implement two stages authentication

--- src/main/java/com/mkh/tutoringplatform/web/controller/auth/AuthController.java
+++ src/main/java/com/mkh/tutoringplatform/web/controller/auth/AuthController.java
@@ -1,55 +1,96 @@
 package com.mkh.tutoringplatform.web.controller.auth;
 
+import com.mkh.tutoringplatform.domain.user.student.Grade;
+import com.mkh.tutoringplatform.domain.user.student.Student;
+import com.mkh.tutoringplatform.domain.user.teacher.Subject;
+import com.mkh.tutoringplatform.domain.user.teacher.Teacher;
 import com.mkh.tutoringplatform.domain.user.user.Role;
 import com.mkh.tutoringplatform.domain.user.user.User;
 import com.mkh.tutoringplatform.service.RegistrationService;
 import com.mkh.tutoringplatform.web.validaton.UserValidator;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import lombok.RequiredArgsConstructor;
+import org.dom4j.rule.Mode;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Controller
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
 
     private final UserValidator userValidator;
     private final RegistrationService registrationService;
 
+    private User createdUser;
+
     @GetMapping("/login")
-    public String loginPage(){
+    public String loginPage() {
         return "auth/login";
     }
 
-    @GetMapping("/registration")
-    public String registrationPage(@ModelAttribute("user") User user, @ModelAttribute("role") String role){
-        return "auth/registration";
+    @GetMapping("/registration/user")
+    public String registrationPage(@ModelAttribute("user") User user, @ModelAttribute("role") String role) {
+        return "auth/user-registration";
     }
 
-    @PostMapping("/registration")
-    public String performRegistration( @ModelAttribute("role") String role, @ModelAttribute("user") @Valid User user, BindingResult bindingResult){
-        if (role.equals("teacher"))
-            user.setRoles(new HashSet<>(List.of(Role.ROLE_TEACHER)));
-        else
-            user.setRoles(new HashSet<>(List.of(Role.ROLE_STUDENT)));
-
+    @PostMapping("/registration/user")
+    public String performRegistration(@ModelAttribute("role") String role, @ModelAttribute("user") @Valid User user, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         userValidator.validate(user, bindingResult);
-        if (bindingResult.hasErrors())
-            return "/auth/registration";
-        registrationService.register(user);
+        if (bindingResult.hasErrors()) {
+            return "auth/user-registration";
+        }
+        createdUser = user;
+        if (role.equals("teacher")) {
+            user.setRoles(new HashSet<>(List.of(Role.ROLE_TEACHER)));
+            return "redirect:/auth/registration/teacher";
+        } else {
+            user.setRoles(new HashSet<>(List.of(Role.ROLE_STUDENT)));
+            return "redirect:/auth/registration/student";
+        }
+    }
+
+    @GetMapping("/registration/teacher")
+    public String registrationPage(@ModelAttribute("teacher") Teacher teacher, Model model) {
+        model.addAttribute("subjects", Subject.values());
+        model.addAttribute("grades", Grade.values());
+        return "auth/teacher-registration";
+    }
+
+    @PostMapping("/registration/teacher")
+    public String performRegistration(@ModelAttribute("teacher") Teacher teacher) {
+        System.out.println(teacher.getGrades());
+        System.out.println(teacher.getSubjects());
+        teacher.setUser(createdUser);
+        registrationService.registerUser(createdUser);
+        registrationService.registerTeacher(teacher);
+        createdUser = null;
         return "redirect:/auth/login";
     }
 
+    @GetMapping("/registration/student")
+    public String registrationPage(@ModelAttribute("student") Student student, Model model) {
+        model.addAttribute("grades", Grade.values());
+        return "auth/student-registration";
+    }
+
+    @PostMapping("/registration/student")
+    public String performRegistration(@ModelAttribute("student") Student student) {
+        student.setUser(createdUser);
+        registrationService.registerUser(createdUser);
+        registrationService.registerStudent(student);
+        createdUser = null;
+        return "redirect:/auth/login";
+    }
 }
