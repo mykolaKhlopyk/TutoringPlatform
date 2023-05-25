ab35fed8fc78ab596e2e105c01aa764c92cf6433
mykola <mykola.khlopyk@gmail.com>
2023-05-25 22:45:49 +0300

    implement Student

--- src/main/java/com/mkh/tutoringplatform/domain/user/student/Student.java
+++ src/main/java/com/mkh/tutoringplatform/domain/user/student/Student.java
@@ -1,9 +1,26 @@
 package com.mkh.tutoringplatform.domain.user.student;
 
 import com.mkh.tutoringplatform.domain.user.user.User;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+import javax.persistence.*;
 
-//@Entity
+@Data
+@Entity
+@Table(name = "students")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @NotNull
+    @Enumerated(EnumType.ORDINAL)
+    private Grade grade;
 
 }
