2696614e13dbf0bc87817066b60c1dbc1934a93f
mykola <mykola.khlopyk@gmail.com>
2023-05-25 22:45:41 +0300

    implement Teacher

--- src/main/java/com/mkh/tutoringplatform/domain/user/teacher/Teacher.java
+++ src/main/java/com/mkh/tutoringplatform/domain/user/teacher/Teacher.java
@@ -1,8 +1,63 @@
 package com.mkh.tutoringplatform.domain.user.teacher;
 
+import com.mkh.tutoringplatform.domain.user.student.Grade;
+import com.mkh.tutoringplatform.domain.user.user.Role;
 import com.mkh.tutoringplatform.domain.user.user.User;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
 
 
-//@Entity
-public class Teacher  {
+@Data
+@Entity
+@Table(name = "teachers")
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Min(value = 0)
+    private int experience;
+
+    @Column(name = "sum_of_votes")
+    private int sumOfVotes;
+
+    @Column(name = "number_of_votes")
+    private int numberOfVotes;
+
+    @Column(name = "pay_per_hour")
+    @Min(value = 0)
+    private int payPerHour;
+
+    @NotNull
+    @Column(name = "registered_at")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Date registeredAt;
+
+    @NotNull
+    @Enumerated(EnumType.ORDINAL)
+    private Place place;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "teachers_grades", joinColumns = @JoinColumn(name = "teacher_id"))
+    @Column(name = "grade_id")
+    private Set<Grade> grades;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "teachers_subjects", joinColumns = @JoinColumn(name = "teacher_id"))
+    @Column(name = "subject_id")
+    private Set<Subject> subjects;
 }
