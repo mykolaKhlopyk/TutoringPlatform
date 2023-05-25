6d3fe7ac99291fbee0d1768992d4940ff4cf7b6f
mykola <mykola.khlopyk@gmail.com>
2023-05-25 22:49:24 +0300

    implement interface for registration

--- src/main/java/com/mkh/tutoringplatform/service/impl/RegistrationServiceImpl.java
+++ src/main/java/com/mkh/tutoringplatform/service/impl/RegistrationServiceImpl.java
@@ -1,6 +1,10 @@
 package com.mkh.tutoringplatform.service.impl;
 
+import com.mkh.tutoringplatform.domain.user.student.Student;
+import com.mkh.tutoringplatform.domain.user.teacher.Teacher;
 import com.mkh.tutoringplatform.domain.user.user.User;
+import com.mkh.tutoringplatform.repository.StudentRepository;
+import com.mkh.tutoringplatform.repository.TeacherRepository;
 import com.mkh.tutoringplatform.repository.UserRepository;
 import com.mkh.tutoringplatform.service.RegistrationService;
 import lombok.AllArgsConstructor;
@@ -9,17 +13,33 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.imageio.spi.RegisterableService;
+import java.util.Date;
 
 @Service
 @AllArgsConstructor
 public class RegistrationServiceImpl implements RegistrationService {
-
     private final UserRepository userRepository;
+    private final TeacherRepository teacherRepository;
+    private final StudentRepository studentRepository;
     private final PasswordEncoder passwordEncoder;
     @Override
-    @Transactional
-    public void register(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+    public void registerUser(User user) {
+        user.setPassword(passwordEncoder.encode( user.getPassword()));
         userRepository.save(user);
     }
+
+    @Override
+    public void registerTeacher(Teacher teacher) {
+        teacher.setNumberOfVotes(0);
+        teacher.setSumOfVotes(0);
+        teacher.setRegisteredAt(new Date());
+        teacherRepository.save(teacher);
+    }
+
+    @Override
+    public void registerStudent(Student student) {
+        studentRepository.save(student);
+    }
+
+
 }
