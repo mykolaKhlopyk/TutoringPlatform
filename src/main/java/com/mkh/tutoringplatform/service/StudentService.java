784285b44b1da25d738e0223d6e919b8df8bd085
mykola <mykola.khlopyk@gmail.com>
2023-05-28 01:57:15 +0300

    create StudentService

---
+++ src/main/java/com/mkh/tutoringplatform/service/StudentService.java
@@ -0,0 +1,9 @@
+package com.mkh.tutoringplatform.service;
+
+import com.mkh.tutoringplatform.domain.user.student.Student;
+
+import java.util.Optional;
+
+public interface StudentService {
+
+}
---
+++ src/main/java/com/mkh/tutoringplatform/service/impl/StudentServiceImpl.java
@@ -0,0 +1,15 @@
+package com.mkh.tutoringplatform.service.impl;
+
+import com.mkh.tutoringplatform.domain.user.student.Student;
+import com.mkh.tutoringplatform.repository.StudentRepository;
+import com.mkh.tutoringplatform.service.StudentService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class StudentServiceImpl implements StudentService {
+
+}
