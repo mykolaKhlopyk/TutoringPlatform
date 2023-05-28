a24bf242ca539df47594a210042f8bc13b0a76f5
mykola <mykola.khlopyk@gmail.com>
2023-05-28 11:47:34 +0300

    implement method request teacher to become tutor for student

--- src/main/java/com/mkh/tutoringplatform/service/StudentService.java
+++ src/main/java/com/mkh/tutoringplatform/service/StudentService.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 
 public interface StudentService {
 
+    void requestTeacher(long id, Student authenticatedStudent);
 }
--- src/main/java/com/mkh/tutoringplatform/service/impl/StudentServiceImpl.java
+++ src/main/java/com/mkh/tutoringplatform/service/impl/StudentServiceImpl.java
@@ -1,15 +1,29 @@
 package com.mkh.tutoringplatform.service.impl;
 
 import com.mkh.tutoringplatform.domain.user.student.Student;
+import com.mkh.tutoringplatform.domain.user.teacher.Teacher;
 import com.mkh.tutoringplatform.repository.StudentRepository;
+import com.mkh.tutoringplatform.repository.TeacherRepository;
 import com.mkh.tutoringplatform.service.StudentService;
 import lombok.AllArgsConstructor;
+import org.hibernate.Hibernate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @AllArgsConstructor
+@Transactional(readOnly = true)
 public class StudentServiceImpl implements StudentService {
-
+    private final TeacherRepository teacherRepository;
+    private final StudentRepository studentRepository;
+    @Override
+    @Transactional
+    public void requestTeacher(long id, Student authenticatedStudent) {
+        Teacher teacher = teacherRepository.findById(id).get();
+        authenticatedStudent = studentRepository.findById(authenticatedStudent.getId()).get();
+        teacher.getNotConfirmedStudents().add(authenticatedStudent);
+        teacherRepository.save(teacher);
+    }
 }
