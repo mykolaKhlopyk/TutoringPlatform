03125aaaaefd5eeeaabe58fc43668133af7bcc27
mykola <mykola.khlopyk@gmail.com>
2023-05-29 13:41:19 +0300

    add methods make request and cansel request in interface

--- src/main/java/com/mkh/tutoringplatform/service/StudentService.java
+++ src/main/java/com/mkh/tutoringplatform/service/StudentService.java
@@ -1,10 +1,15 @@
 package com.mkh.tutoringplatform.service;
 
 import com.mkh.tutoringplatform.domain.user.student.Student;
+import com.mkh.tutoringplatform.domain.user.teacher.Teacher;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentService {
 
     void requestTeacher(long id, Student authenticatedStudent);
+    List<Teacher> getAllRequestedByStudent(Student student);
+
+    void cancelRequest(long id, Student authenticatedStudent);
 }
