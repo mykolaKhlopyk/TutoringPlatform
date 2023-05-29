1f9736ab35af701027924a5e61036ae9e4b27048
mykola <mykola.khlopyk@gmail.com>
2023-05-29 13:22:50 +0300

    implement class Group what contains students

---
+++ src/main/java/com/mkh/tutoringplatform/domain/user/student/Group.java
@@ -0,0 +1,36 @@
+package com.mkh.tutoringplatform.domain.user.student;
+
+import com.mkh.tutoringplatform.domain.user.student.Student;
+import com.mkh.tutoringplatform.domain.user.teacher.Teacher;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "groups")
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Size(min=2, max = 10, message = "Group should be between 6 and 16 characters")
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "students_groups",
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id"))
+    private List<Student> students;
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id", referencedColumnName = "id")
+    private Teacher teacher;
+
+
+}
