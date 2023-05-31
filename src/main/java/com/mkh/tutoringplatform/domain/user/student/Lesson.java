0402736bf1f5e37af08116fa02e9caa987dffbd2
mykola <mykola.khlopyk@gmail.com>
2023-05-31 18:23:04 +0300

    update date in Lesson

--- src/main/java/com/mkh/tutoringplatform/domain/user/student/Lesson.java
+++ src/main/java/com/mkh/tutoringplatform/domain/user/student/Lesson.java
@@ -1,11 +1,16 @@
 package com.mkh.tutoringplatform.domain.user.student;
 
-import jakarta.validation.constraints.NotNull;
+import com.mkh.tutoringplatform.domain.user.teacher.Subject;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import java.time.Duration;
-import java.time.LocalDateTime;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 @Entity
 @Data
@@ -15,13 +20,36 @@ public class Lesson {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    private Duration duration;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Subject subject;
 
     @NotNull
+    //@Size(min = 10, max = 300, message = "durationq is incorrect")
+    @Column(name = "duration_minutes")
+    private int duration;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     @Column(name = "time_start")
-    private LocalDateTime timeStart;
+    private Date timeStart;
+    @Transient
+    private Date timeFinish;
 
     @ManyToOne
     @JoinColumn(name = "group_id", referencedColumnName = "id")
     private Group group;
+
+    public Date getTimeFinish() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(getTimeStart());
+        calendar.add(Calendar.MINUTE, getDuration());
+        return calendar.getTime();
+    }
+
+    public String getStringTimeFinish() {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return format.format(getTimeFinish());
+    }
 }
